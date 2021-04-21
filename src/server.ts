126323dc787ddaa178f494a68f99b441c490e461
Pedro Poglia <pedro.poglia@hotmail.com>
2021-04-21 18:19:45 -0300

    NLW05 Aula 02

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,12 @@
-import express, { response } from 'express';
+import express from 'express';
+import "./database";
+import {routes} from './routes'
 
 const app = express();
 
+app.use(express.json());
 
-
-/*
-    GET : buscas
-    POST : criação
-    PUT : alteração
-    DELETE : deletar
-    PATH : alterar uma informação especifica 
-
-*/
-
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Olá NLW 05",
-    });
-});
-
-app.post("/users" ,(request, response) => {
-    return response.json({
-        messsage: "Usuario salvo com sucesso",
-    });
-});
+app.use(routes);
 
 
 app.listen(3333, () => console.log("Servers is runing on port 3333"));
