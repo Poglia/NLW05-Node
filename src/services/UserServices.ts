ebf664ced2c6f29bd5e7dc27f681cc50606f042c
Pedro Poglia <pedro.poglia@hotmail.com>
2021-04-23 03:14:18 -0300

    NLW05 Aula 03

---
+++ src/services/UserServices.ts
@@ -0,0 +1,39 @@
+import { getCustomRepository, Repository} from 'typeorm'
+import { UsersRepository } from '../repositories/UsersRepository'
+import { User } from '../entities/User'
+
+class UsersService {
+
+    private usersRepository: Repository<User>
+
+    constructor() {
+        this.usersRepository = getCustomRepository(UsersRepository)
+    }
+
+    async create(email: string) {
+   
+        // Verificar se usuario existe
+
+        const userExists = await this.usersRepository.findOne({
+            email,
+        
+        });
+
+        // Se existir, retornar user 
+        if(userExists) {
+            return userExists;
+        }
+
+        const user = this.usersRepository.create({
+            email,
+        });
+
+        await this.usersRepository.save(user);
+
+        // Se não existir, salvar no BD
+
+        return user;
+    }
+}
+
+export {UsersService}
